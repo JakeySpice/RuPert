a396c4da7c633b0e715b82421e1cad0c9e20c2ee
Jake Spicer <JakeCSpicer@gmail.com>
2023-03-31 17:53:47 +1100

    Added response stop instruction, made GUI look slightly prettier

--- RuPertGUI.py
+++ RuPertGUI.py
@@ -1,12 +1,16 @@
+import os
 import tkinter as tk
 from tkinter import ttk
 
-import os
-# If you want to add your own API key, uncomment the following line and add your API key in the quotes. 
-# Just make sure to delete before committing to GitHub.
-# os.environ["OPENAI_API_KEY"] = "<your API key>" 
+
+
+# This calls the config.py file to get the OpenAI API key. config.py is not included in this repo
+# because it contains the OpenAI API key. You can create your own config.py file and add the
+# OpenAI API key to it.
 from config import OPENAI_API_KEY
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY 
+
+# Set the OpenAI API key
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 import openai
 
 def generate_response(prompt):
@@ -16,38 +20,40 @@ def generate_response(prompt):
         prompt=prompt,
         max_tokens=50,
         n=1,
-        stop=None,
+        stop="\n\n",
         temperature=0.7,
     )
 
     return response.choices[0].text.strip()
 
+
 def on_button_click():
     input_text = entry.get("1.0", "end-1c")
-    # Call your finetuned OpenAI model here with input_text as the input
     output_text = generate_response(input_text)
     result_text.delete('1.0', tk.END)
     result_text.insert(tk.END, output_text)
 
-# Create the main window
-root = tk.Tk()
-root.title("RuPert v0.1")
 
-# Create an input field (Text widget)
-entry = tk.Text(root, wrap="word", width=50, height=5)
-entry.grid(row=0, column=0, padx=10, pady=10, sticky="N")
+def create_main_window():
+    root = tk.Tk()
+    root.title("RuPert v0.1")
 
-# Create a button
-button = ttk.Button(root, text="Submit", command=on_button_click)
-button.grid(row=1, column=0, padx=10, pady=10, sticky="N")
+    entry = tk.Text(root, wrap="word", width=50, height=5)
+    entry.grid(row=0, column=0, padx=10, pady=10, sticky="N")
 
-# Create an output text field
-result_text = tk.Text(root, wrap="word", width=50, height=10)
-result_text.grid(row=2, column=0, columnspan=2, padx=10, pady=10, sticky="N")
-# Create a scrollbar
-scrollbar = ttk.Scrollbar(root, orient="vertical", command=result_text.yview)
-scrollbar.grid(row=2, column=1, sticky="NS")
-result_text["yscrollcommand"] = scrollbar.set
+    button = ttk.Button(root, text="Submit", command=on_button_click)
+    button.grid(row=1, column=0, padx=10, pady=10, sticky="N")
 
-# Run the main loop
-root.mainloop()
+    result_text = tk.Text(root, wrap="word", width=50, height=10)
+    result_text.grid(row=2, column=0, columnspan=2, padx=10, pady=10, sticky="N")
+
+    scrollbar = ttk.Scrollbar(root, orient="vertical", command=result_text.yview)
+    scrollbar.grid(row=2, column=1, sticky="NS")
+    result_text["yscrollcommand"] = scrollbar.set
+
+    return root, entry, result_text
+
+
+if __name__ == "__main__":
+    root, entry, result_text = create_main_window()
+    root.mainloop()
